987e85ba1d383a6bccd096bdd03e7b2d9499d56f
Natalia <natib13@gmail.com>
2022-12-16 14:16:22 +0100

    fix

--- productions/P7.py
+++ productions/P7.py
@@ -2,29 +2,31 @@ from utils.StandardizedGraph import StandardizedGraph, Vert
 
 def P7(graph: StandardizedGraph, v0: Vert, v1: Vert,v2: Vert ):
 
+    # find vertices on the same position
     v0_vertices = graph.find_by_pos(v0.pos_x(), v0.pos_y())
     v1_vertices = graph.find_by_pos(v1.pos_x(), v1.pos_y())
     v2_vertices = graph.find_by_pos(v2.pos_x(), v2.pos_y())
 
-
+    # find neighbours of one vertex on each position
     v0_0_neigh = (graph.get_neighbours(v0_vertices[0], v0.level(), label = "I") +
                  graph.get_neighbours(v0_vertices[0], v0.level(), label = "E"))
-    v0_0_neigh.remove(v1.underlying)
+    v0_0_neigh.remove(v1_vertices[0].underlying)
 
     v1_0_neigh = (graph.get_neighbours(v1_vertices[0], v1.level(), label="I") +
                   graph.get_neighbours(v1_vertices[0], v1.level(), label="E"))
-
-    v1_0_neigh.remove(v0.underlying)
-    v1_0_neigh.remove(v2.underlying)
+    v1_0_neigh.remove(v0_vertices[0].underlying)
+    v1_0_neigh.remove(v2_vertices[0].underlying)
 
     v2_0_neigh = (graph.get_neighbours(v2_vertices[0], v2.level(), label="I") +
                  graph.get_neighbours(v2_vertices[0], v2.level(), label="E"))
-    v2_0_neigh.remove(v1.underlying)
+    v2_0_neigh.remove(v1_vertices[0].underlying)
 
+    # remove one repeated vertex
     graph.remove_vertex(v0_vertices[0])
     graph.remove_vertex(v1_vertices[0])
     graph.remove_vertex(v2_vertices[0])
 
+    # add edges for remaining vertex
     for neigh_0 in v0_0_neigh:
         graph.add_edge(v0_vertices[1], Vert(graph.underlying, neigh_0))
 
@@ -36,5 +38,7 @@ def P7(graph: StandardizedGraph, v0: Vert, v1: Vert,v2: Vert ):
 
     return graph
 
-def match_P7(graph: StandardizedGraph):
-    return False
+def match_P7(graph: StandardizedGraph, level:int):
+    # graph.in
+
+    return []
--- productions/P8.py
+++ productions/P8.py
@@ -5,18 +5,14 @@ def P8(graph: StandardizedGraph, v0: Vert, v1: Vert, v2:Vert ):
     v1_vertices = graph.find_by_pos(v1.pos_x(), v1.pos_y())
     v2_vertices = graph.find_by_pos(v2.pos_x(), v2.pos_y())
 
-    print(graph.get_neighbours(v1_vertices[0], v0.level(), label="E"))
-    print(graph.get_neighbours(v2_vertices[0], v0.level(), label="I"))
-
     v1_0_neigh = (graph.get_neighbours(v1_vertices[0], v1.level(), label="I") +
                   graph.get_neighbours(v1_vertices[0], v1.level(), label="E"))
-
     v1_0_neigh.remove(v0.underlying)
-    v1_0_neigh.remove(v2.underlying)
+    v1_0_neigh.remove(v2_vertices[0].underlying)
 
     v2_0_neigh = (graph.get_neighbours(v2_vertices[0], v2.level(), label="I") +
                   graph.get_neighbours(v2_vertices[0], v2.level(), label="E"))
-    v2_0_neigh.remove(v1.underlying)
+    v2_0_neigh.remove(v1_vertices[0].underlying)
 
     graph.remove_vertex(v1_vertices[0])
     graph.remove_vertex(v2_vertices[0])
@@ -30,5 +26,6 @@ def P8(graph: StandardizedGraph, v0: Vert, v1: Vert, v2:Vert ):
 
     return graph
 
-def match_P8(graph: StandardizedGraph):
+def match_P8(graph: StandardizedGraph, level: int):
+
     return False
